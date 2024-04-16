d8cb08d35797ea49c5cfc31e16691ab0bdd11da3
wael <wael@dinweldik.com>
2024-04-16 15:13:34 +0200

    style: improve remove button styling

--- src/lib/components/CryptoCard.svelte
+++ src/lib/components/CryptoCard.svelte
@@ -19,27 +19,28 @@
 		<div class="balance">
 			<h1><strong>{crypto.type}</strong>{crypto.balance}</h1>
 			<h2>{crypto.usd}</h2>
+			<button class="remove-icon" on:click={remove}>remove</button>
 		</div>
 		<div class="info">
 			<h2>{crypto.addr}</h2>
 		</div>
 	</div>
-	<div class="footer">
-		<button on:click={remove}>delete</button>
-	</div>
 </div>
 
 <style>
 	.card {
+		position: relative;
 		border: 1px solid var(--color-primary);
 		padding: var(--size-4);
 		border-radius: var(--size-2);
 	}
-	.footer {
-		width: 100%;
-		display: flex;
-		justify-content: center;
-		margin-top: var(--size-4);
+	.remove-icon {
+		inherits: none;
+		cursor: pointer;
+		background-color: var(--color-primary);
+		font: var(--text-md);
+		width: 50%;
+		border-radius: var(--size-2);
 	}
 	.card-content {
 		display: grid;
@@ -50,15 +51,9 @@
 		text-transform: uppercase;
 		display: grid;
 		grid-template-columns: 1fr;
-		gap: var(--size-3);
-
+		align-self: flex-start;
 		& h1 {
 			font-weight: normal;
-			align-self: start; /* Aligns this item to the top of the container */
-		}
-
-		& h2 {
-			align-self: end; /* Aligns this item to the bottom of the container */
 		}
 	}
 	.info {
