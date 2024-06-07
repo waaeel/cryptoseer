a94a6b1cf280835a579b9e89010480a46faa8649
wael <wael@dinweldik.com>
2024-06-07 15:07:14 +0200

    feat: add quick navigation button to wallet page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,8 +3,25 @@
 </script>
 
 <header>
+	<div class="external-apps">
+		<a class="btn" href="/app">Go to Wallet</a>
+	</div>
 	<Hero />
 </header>
 
 <style lang="postcss">
+	.external-apps {
+		padding: var(--size-2) var(--size-content-px);
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-end;
+		gap: var(--size-8);
+		background: var(--color-primary);
+		& p,
+		& a {
+			color: var(--color-black);
+			font-weight: bold;
+			text-decoration: none;
+		}
+	}
 </style>
