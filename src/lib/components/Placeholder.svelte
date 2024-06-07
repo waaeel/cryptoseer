b2f9deb64bc261a7b578bbb9306d9fb7d2ab3c53
wael <wael@dinweldik.com>
2024-06-07 15:06:28 +0200

    feat: add empty-state placeholder component

---
+++ src/lib/components/Placeholder.svelte
@@ -0,0 +1,26 @@
+<script>
+	// Any necessary script for your placeholder
+</script>
+
+<div class="placeholder">
+	<h2 class="title">No Wallets Added</h2>
+	<p>Add a public key to start tracking your crypto assets.</p>
+</div>
+
+<style>
+	.placeholder {
+		text-align: center;
+		padding: var(--size-6);
+	}
+
+	.title {
+		font-size: 2.25rem;
+		font-weight: bold;
+		letter-spacing: -0.025em;
+	}
+
+	p {
+		font-size: 1.25rem;
+		margin-bottom: var(--size-4);
+	}
+</style>
