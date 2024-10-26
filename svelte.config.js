eacdc6fbec9618d928e6052e010348aade6ef2ea
wael <wael@dinweldik.com>
2024-10-26 17:21:00 +0200

    fix: correct caching configuration

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,17 @@ import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
+import { execSync } from 'child_process';
+
+// Function to get the current Git commit hash
+function getGitRevision() {
+	try {
+		return execSync('git rev-parse --short HEAD').toString().trim();
+	} catch (e) {
+		console.warn('Unable to get Git revision, using current timestamp');
+		return Date.now().toString();
+	}
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,12 +28,15 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
-
-		// remove this if you don't want prerendering
+		adapter: adapter(),
+		// TODO remove this if you don't want prerendering
 		/* 	prerender: {
 			entries: ['*', '/sitemap.xml', '/rss.xml']
 		} */
+		version: {
+			name: getGitRevision(),
+			pollInterval: 0
+		}
 	}
 };
 
