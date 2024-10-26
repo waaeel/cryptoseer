8d65cdaa92cf4dac7a4855228351e465b43869f9
wael <wael@dinweldik.com>
2024-10-26 17:27:06 +0200

    chore: update service worker behavior

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,6 @@
 /// <reference types="@sveltejs/kit" />
 import { build, files, version } from '$service-worker';
 
-// Create a unique cache name for this deployment
 const CACHE = `cache-${version}`;
 
 const ASSETS = [
@@ -11,40 +10,36 @@ const ASSETS = [
 
 self.addEventListener('install', (event) => {
 	// Create a new cache and add all files to it
-	/* 	async function addFilesToCache() {
+	async function addFilesToCache() {
 		const cache = await caches.open(CACHE);
 		await cache.addAll(ASSETS);
 	}
-
-	event.waitUntil(addFilesToCache()); */
+	event.waitUntil(addFilesToCache());
 });
 
 self.addEventListener('activate', (event) => {
-	/* 	// Remove previous cached data from disk
+	// Remove previous cached data from disk if the version has changed
 	async function deleteOldCaches() {
 		for (const key of await caches.keys()) {
 			if (key !== CACHE) await caches.delete(key);
 		}
 	}
-
-	event.waitUntil(deleteOldCaches()); */
+	event.waitUntil(deleteOldCaches());
 });
 
 self.addEventListener('fetch', (event) => {
-	// ignore POST requests etc
 	if (event.request.method !== 'GET') return;
 
 	async function respond() {
 		const url = new URL(event.request.url);
 		const cache = await caches.open(CACHE);
 
-		// `build`/`files` can always be served from the cache
-		/* 		if (ASSETS.includes(url.pathname)) {
+		// Serve assets from cache or add to cache if fetched from network
+		if (ASSETS.includes(url.pathname)) {
 			return cache.match(url.pathname);
-		} */
+		}
 
-		// for everything else, try the network first, but
-		// fall back to the cache if we're offline
+		// Try network first, fall back to cache if offline
 		try {
 			const response = await fetch(event.request);
 
